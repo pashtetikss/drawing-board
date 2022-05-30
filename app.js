0f098f029221481a94c31c36f47f33fb0de645fe
pashtetikss <105599592+pashtetikss@users.noreply.github.com>
2022-05-30 17:22:14 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+const board = document.querySelector("#board")
+const  SQUARES_NUMBER = 500
+const colors = ["red", "blue", "green", "yellow", "purple"]
+const screen = window.matchMedia("(max-width:768px)")
+if (screen.matches) {
+    for (let i=0; i<SQUARES_NUMBER; i++){
+        const square = document.createElement("div")
+        square.classList.add("square")
+    
+        square.addEventListener("mouseover", () => 
+           // console.log("adawdwadwa")
+            setColor(square))
+    
+        square.addEventListener("mouseleave", () => 
+        // console.log("adawdwadwa")
+         removeColor(square))
+    
+        board.append(square)
+    }
+    
+}
+else{
+
+for (let i=0; i<SQUARES_NUMBER; i++){
+    const square = document.createElement("div")
+    square.classList.add("square")
+
+    square.addEventListener("mouseover", () => 
+       // console.log("adawdwadwa")
+        setColor(square))
+
+    square.addEventListener("mouseleave", () => 
+    // console.log("adawdwadwa")
+     removeColor(square))
+
+    board.append(square)
+}
+}
+
+function setColor(element){
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+
+}
+
+function removeColor(element){
+    element.style.backgroundColor = "#1d1d1d"
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor(){
+   const index = Math.floor(Math.random() * colors.length)
+   return colors[index]
+}
+
+
